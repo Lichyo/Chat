df0c3416ca852ad3d52fab73a926135ccada5678
Lichyo <chyoxuan@gmail.com>
2023-02-01 00:09:09 +0800

    Basic Chat UI Finished.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,28 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final _firestore = FirebaseFirestore.instance;
 
 class ChatScreen extends StatefulWidget {
   static String id = 'chat_screen';
+
+  const ChatScreen({super.key});
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
   @override
-
   final _auth = FirebaseAuth.instance;
   late User loggedInUser;
+  late String messageText;
+  late String? senderEmail;
 
   void getCurrentUser() async {
     try {
-      final user = await _auth.currentUser;
+      final user = _auth.currentUser;
       if (user != null) {
         loggedInUser = user;
-        print(loggedInUser.email);
+        senderEmail = loggedInUser.email;
       }
     } catch (e) {
       print(e);
+    }
+  }
+
+  void messageStream() async {
+    await for (var snapshot in _firestore.collection('messages').snapshots()) {
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
     }
   }
 
@@ -40,8 +54,9 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: const Icon(Icons.close),
               onPressed: () {
-                _auth.signOut();
-                Navigator.pop(context);
+                // _auth.signOut();
+                // Navigator.pop(context);
+                messageStream();
               }),
         ],
         title: const Text('⚡️Chat'),
@@ -52,6 +67,32 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot>(
+              stream: _firestore.collection('messages').snapshots(),
+              builder: (context, snapshot) {
+                List<Text> messageWidgets = [];
+                if (snapshot.hasData) {
+                  final messages = snapshot.data!.docs;
+                  for (var message in messages) {
+                    final messageText = message.get('text');
+                    final messageSender = message.get('senderEmail');
+                    final messageWidget = Text(
+                      '$messageSender say : $messageText.',
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    );
+                    messageWidgets.add(messageWidget);
+                  }
+                }
+                return Expanded(
+                  child: ListView(
+                    padding: const EdgeInsets.all(15.0),
+                    children: messageWidgets,
+                  ),
+                );
+              },
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -60,14 +101,17 @@ class _ChatScreenState extends State<ChatScreen> {
                   Expanded(
                     child: TextField(
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   TextButton(
-                    onPressed: () {
-                      //Implement send functionality.
+                    onPressed: () async {
+                      await _firestore.collection('messages').add({
+                        'text': messageText,
+                        'senderEmail': senderEmail,
+                      });
                     },
                     child: const Text(
                       'Send',
@@ -79,6 +123,20 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  const MessageBubble({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Material(
+      color: Colors.lightBlue,
+      child: Padding(
+        padding: EdgeInsets.all(15.0),
       ),
     );
   }
